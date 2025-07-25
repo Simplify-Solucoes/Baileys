00187a90e236e737c1d1712fabc25fda22e12975
Gustavo Quadri <87215048+gusquadri@users.noreply.github.com>
2025-07-25 12:36:40 -0300

    fix: crypto.ts migrated to typescript libsignal

--- src/Utils/crypto.ts
+++ src/Utils/crypto.ts
@@ -1,6 +1,5 @@
 import { createCipheriv, createDecipheriv, createHash, createHmac, randomBytes } from 'crypto'
-/* @ts-ignore */
-import * as libsignal from 'libsignal'
+import { curve, crypto as libsignalCrypto } from '@wppconnect-team/libsignal-protocol'
 import { KEY_BUNDLE_TYPE } from '../Defaults'
 import type { KeyPair } from '../Types'
 
@@ -13,7 +12,7 @@ export const generateSignalPubKey = (pubKey: Uint8Array | Buffer) =>
 
 export const Curve = {
 	generateKeyPair: (): KeyPair => {
-		const { pubKey, privKey } = libsignal.curve.generateKeyPair()
+		const { pubKey, privKey } = curve.createKeyPair(libsignalCrypto.getRandomBytes(32))
 		return {
 			private: Buffer.from(privKey),
 			// remove version byte
@@ -21,14 +20,13 @@ export const Curve = {
 		}
 	},
 	sharedKey: (privateKey: Uint8Array, publicKey: Uint8Array) => {
-		const shared = libsignal.curve.calculateAgreement(generateSignalPubKey(publicKey), privateKey)
+		const shared = curve.ECDHE(generateSignalPubKey(publicKey), privateKey)
 		return Buffer.from(shared)
 	},
-	sign: (privateKey: Uint8Array, buf: Uint8Array) => libsignal.curve.calculateSignature(privateKey, buf),
+	sign: (privateKey: Uint8Array, buf: Uint8Array) => curve.Ed25519Sign(privateKey, buf),
 	verify: (pubKey: Uint8Array, message: Uint8Array, signature: Uint8Array) => {
 		try {
-			libsignal.curve.verifySignature(generateSignalPubKey(pubKey), message, signature)
-			return true
+			return curve.Ed25519Verify(generateSignalPubKey(pubKey), message, signature)
 		} catch (error) {
 			return false
 		}
