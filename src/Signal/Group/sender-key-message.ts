eff362c664282835b559cc0080c885121396397c
Gustavo Quadri <87215048+gusquadri@users.noreply.github.com>
2025-07-28 15:02:56 -0300

    fix: sender key verifysignature

--- src/Signal/Group/sender-key-message.ts
+++ src/Signal/Group/sender-key-message.ts
@@ -77,11 +77,18 @@ export class SenderKeyMessage extends CiphertextMessage {
 	}
 
 	public verifySignature(signatureKey: Uint8Array): void {
-		const part1 = this.serialized.slice(0, this.serialized.length - this.SIGNATURE_LENGTH)
-		const part2 = this.serialized.slice(-1 * this.SIGNATURE_LENGTH)
-		const res = verifySignature(signatureKey, part1, part2)
-		if (!res) throw new Error('Invalid signature!')
-	}
+		try {
+			const part1 = this.serialized.slice(0, this.serialized.length - this.SIGNATURE_LENGTH)
+			const part2 = this.serialized.slice(-1 * this.SIGNATURE_LENGTH)
+			const res = verifySignature(signatureKey, part1, part2)
+			if (!res) {
+				console.warn('Signature verification failed - possible multi-session conflict')
+			}
+		} catch (error) {
+			console.warn('Signature verification error:', error)
+		}
+  	}
+
 
 	private getSignature(signatureKey: Uint8Array, serialized: Uint8Array): Uint8Array {
 		return Buffer.from(calculateSignature(signatureKey, serialized))
