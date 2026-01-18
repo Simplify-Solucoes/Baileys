2c758c4aa7bf09e281ceadf5d912976cdc62394f
Gustavo Quadri <87215048+gusquadri@users.noreply.github.com>
2026-01-18 16:48:07 -0300

    Revert "test: log lidmap"
    This reverts commit 154b3ea79a3eb420284ef342d192bb783884f14c.

--- src/Utils/event-buffer.ts
+++ src/Utils/event-buffer.ts
@@ -284,11 +284,6 @@ function append<E extends BufferableEvent>(
 					absorbingChatUpdate(chat)
 				}
 			}
-			
-			for (const lidmap of eventData.historyLidMap || []) {
-				// Just log it for now
-				logger.trace({ lidmap }, 'Received history LID mapping')
-			}
 
 			for (const contact of eventData.contacts as Contact[]) {
 				const existingContact = data.historySets.contacts[contact.id]
